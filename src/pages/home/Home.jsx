9bd85e16224a1dc7b3c9ffdaea9664a038f97b17
High_Lander <codywmarkham@gmail.com>
2021-10-29 00:37:41 -0500

    setting up components "left,right,feed"

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import Topbar from "../../components/topbar/Topbar";
+import Leftbar from "../../components/leftbar/Leftbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+import "./home.css"
+
+export default function Home() {
+    return (
+        <>
+        <Topbar/>
+        <div className="homeContainer"></div>
+        <Leftbar/>
+        <Feed/>
+        <Rightbar/>
+        </>
+    )
+}
