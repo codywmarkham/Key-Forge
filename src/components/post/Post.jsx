853bf47d101be41deffd6d9824a604a2571d96d5
High_Lander <codywmarkham@gmail.com>
2021-10-29 11:51:50 -0500

    set up profiles for feed and accounts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,30 +1,36 @@
 import "./post.css"
 import {MoreVert} from "@material-ui/icons"
-export default function Post() {
+import {Users} from "../../dummyData"
+
+
+export default function Post({post}) {
+
+    const user = Users.filter(u=>u.id===1)
+
     return (
         <div className="post">
           <div className="postWrapper">
               <div className="postTop">
                     <div className="postTopLeft">
-                          <img className="postProfileImg" src="/assets/codyhs.png" alt="" />
-                          <span className="postUsername">Cody Markham</span>
-                          <span className="postDate">7 mins ago</span>
+                          <img className="postProfileImg" src={Users.filter(u=>u.id === post.userId)[0].profilePicture} alt="" />
+                          <span className="postUsername">{Users.filter(u=>u.id === post.userId)[0].username}</span>
+                          <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                             <MoreVert/>
                     </div>
               </div>
               <div className="postCenter">
-                  <span className="postText">Check out my Notetaker app!</span>
-                  <img className="postImg" src="/assets/notetaker.png" alt="" />
+                  <span className="postText">{post?.desc}</span>
+                  <img className="postImg" src={post.photo} alt="" />
               </div>
               <div className="postBottom">
                   <div className="postBottomLeft">
                       <img className="likeIcon" src="/assets/like.png" alt="" />
-                      <span className="postLikeCounter"> 27 Likes</span>
+                      <span className="postLikeCounter">{post.like}</span>
                   </div>
                   <div className="postBottomRight">
-                      <span className="postCommentText">9 Comments</span>
+                      <span className="postCommentText">{post.comment}</span>
                   </div>
               </div>
           </div>
